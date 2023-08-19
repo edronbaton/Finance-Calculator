6449de7a0640b1550134131866b826fa1cd7526a
EdroNbatoN <140645178+EdroNbatoN@users.noreply.github.com>
2023-08-19 23:15:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -84,8 +84,8 @@ window.addEventListener('DOMContentLoaded', function () {
     countBtn.addEventListener('click', function () {
         if (checkStart()) { 
             appData.moneyPerDay = (appData.budget / 30 ).toFixed(1);
-            console.log(appData.moneyPerDay)
-            dayBudgetValue.textContent = appData.moneyPerDay.toFixed(1)+ 'UAH'
+
+            dayBudgetValue.textContent = appData.moneyPerDay+ 'UAH'
             if (appData.moneyPerDay < 100) {
                 levelValue.textContent = 'Минимальный уровень достатка'
             }   else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
